75b526029fed27ad6dda7c54e8278a9826608aab
lci104u <lci104u@cs.ccu.edu.tw>
2019-03-31 21:29:07 +0800

    140$/hr --> 150$/hr
    <(_ _)>

--- CNA_Pay/requester.py
+++ CNA_Pay/requester.py
@@ -156,7 +156,7 @@ def submitPayment(year,month,days):
     #submit payment
     payload={
         'chka':'off',
-        'hour_money':'140',
+        'hour_money':'150',
         'sutype':'1',
         'iswork':'0',
         'emp_type':'1',
